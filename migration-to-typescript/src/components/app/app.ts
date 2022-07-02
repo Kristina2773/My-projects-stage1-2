73334e0007a3e3c34f4f60f6c8673fad68fb3f75
Kristina <d.kristina27@yandex.ru>
2022-07-02 13:37:57 +0400

    fix: configure eslint

--- migration-to-typescript/src/components/app/app.ts
+++ migration-to-typescript/src/components/app/app.ts
@@ -1,6 +1,5 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
-import {IDataNews, IData, ICallBack} from '../types/types';
 
 class App {
     controller: AppController;
